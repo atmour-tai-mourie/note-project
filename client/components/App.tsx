5afaa6bf6829d52b7af86c0c5c74625b701d4741
atmour-tai-mourie <taii.mourie@gmail.com>
2022-05-26 17:43:04 +1200

    add app component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect } from 'react'
+import Board from './Board'
+import Header from './Header'
+import Main from './Main'
 
 function App(): JSX.Element {
   return (
     <>
       <div className="app">
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
+        <Header />
+        <Main />
       </div>
     </>
   )
