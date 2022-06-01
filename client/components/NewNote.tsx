ae3b693ec9d69eba0ba594e3f1629a3d8a217961
atmour-tai-mourie <taii.mourie@gmail.com>
2022-06-01 23:36:08 +1200

    add bad code for drag function and links

--- client/components/NewNote.tsx
+++ client/components/NewNote.tsx
@@ -2,6 +2,7 @@ import React, { ChangeEvent, FormEvent, useState } from 'react'
 
 function NewNote(): JSX.Element {
   // need a blank note to be automatically removed i.e once unselected the note disapears
+  interface mousePositions {pos1: number, pos2: number, pos3: number, pos4: number}
 
   const [newNote, setNewNote] = useState({ title: '', entry: '' })
 
@@ -26,7 +27,43 @@ function NewNote(): JSX.Element {
   // need to create function/ logic to prevent new notes being created
   function handleNoteClick(event: React.MouseEvent) {
     event.stopPropagation()
+    const mousePositions = {pos1: 0, pos2: 0, pos3: 0, pos4: 0}
+    dragMouseDown(event, mousePositions)
   }
+
+
+  //https://stackoverflow.com/questions/61292302/move-able-drag-able-div-in-react
+  // https://javascript.plainenglish.io/how-to-make-a-simple-custom-usedrag-react-hook-6b606d45d353s
+  function dragMouseDown(event: React.MouseEvent, mousePositions: mousePositions) {
+    event = event || window.event;
+    event.preventDefault();
+    // get the mouse cursor position at startup:
+    mousePositions.pos3 = event.clientX;
+    mousePositions.pos4 = event.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e: React.MouseEvent,  mousePositions: mousePositions) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    mousePositions.pos1 = mousePositions.pos3 - e.clientX;
+    mousePositions.pos2 = mousePositions.pos4 - e.clientY;
+    mousePositions.pos3 = e.clientX;
+    mousePositions.pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+function closeDragElement() {
+  // stop moving when mouse button is released:
+  document.onmouseup = null;
+  document.onmousemove = null;
+}
+}
 
   return (
     <article className="note" onClick={handleNoteClick}>
