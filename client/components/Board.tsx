6c69033634f8f42a0ac2040ba59dca2a1f9d74d2
atmour-tai-mourie <taii.mourie@gmail.com>
2022-06-08 11:59:57 +1200

    grab mouse position for new note

--- client/components/Board.tsx
+++ client/components/Board.tsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react'
 import NewNote from './NewNote'
 
+// NEED TO GIVE BOARD fixed size so postnotes have fixed points
+// ability to move / zoom easily
+
 function Board(): JSX.Element {
   const [showNewNote, setShowNewNote] = useState(false)
   // need to remove initial state and set type for notes explicitly
-  const [notes, setNotes] = useState([{ title: '', entry: '' }])
+  const [notes, setNotes] = useState([
+    { title: '', entry: '', startingPostion: [0, 0] },
+  ])
 
-  function handleMainClick() {
-    setNotes([...notes, { title: '', entry: '' }])
+  function handleMainClick(event: React.MouseEvent) {
+    setNotes([
+      ...notes,
+      { title: '', entry: '', startingPostion: [event.clientX, event.clientY] },
+    ])
+    event
   }
 
   return (
     <section className="board" onClick={handleMainClick}>
-      {notes.map(() => (
-        <NewNote />
+      {notes.map((note) => (
+        <NewNote startingPosition={note.startingPostion} />
       ))}
     </section>
   )
